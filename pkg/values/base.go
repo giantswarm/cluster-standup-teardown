6d25c0efa12c31a4e8b0ccf161cb6c54d5a6b238
Marcus Noble <AverageMarcus@users.noreply.github.com>
2024-05-20 12:52:39 +0100

    Include PipelineRun and Taskrun name in Cluster labels (#29)
    * Include PipelineRun and Taskrun name in Cluster labels
    
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>
    
    * Fix struct tag
    
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>
    
    ---------
    
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

---
+++ pkg/values/base.go
@@ -0,0 +1,43 @@
+package values
+
+import (
+	"os"
+
+	"sigs.k8s.io/yaml"
+)
+
+type baseValues struct {
+	Global global `json:"global" yaml:"global"`
+}
+
+type global struct {
+	Metadata metadata `json:"metadata" yaml:"metadata"`
+}
+
+type metadata struct {
+	Labels map[string]string `json:"labels" yaml:"labels"`
+}
+
+// BuildBaseValues generates a base set of cluster values that are relevant for all test clusters
+func BuildBaseValues() string {
+	v := baseValues{
+		Global: global{
+			Metadata: metadata{
+				Labels: map[string]string{
+					"monitoring.giantswarm.io/prometheus-volume-size": "small",
+				},
+			},
+		},
+	}
+
+	// If found, populate details about Tekton run as labels
+	if os.Getenv("TEKTON_PIPELINE_RUN") != "" {
+		v.Global.Metadata.Labels["cicd.giantswarm.io/pipelinerun"] = os.Getenv("TEKTON_PIPELINE_RUN")
+	}
+	if os.Getenv("TEKTON_TASK_RUN") != "" {
+		v.Global.Metadata.Labels["cicd.giantswarm.io/taskrun"] = os.Getenv("TEKTON_TASK_RUN")
+	}
+
+	bytes, _ := yaml.Marshal(v)
+	return string(bytes)
+}
