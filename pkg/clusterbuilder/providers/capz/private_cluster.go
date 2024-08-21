066a9572cbed38fefad302b488959a099653a369
Puru <5674762+tuladhar@users.noreply.github.com>
2024-08-21 18:59:12 +0545

    Refactor and update CHANGELOG

--- pkg/clusterbuilder/providers/capz/private_cluster.go
+++ pkg/clusterbuilder/providers/capz/private_cluster.go
@@ -33,26 +33,16 @@ func (c *PrivateClusterBuilder) NewClusterApp(clusterName string, orgName string
 		orgName = utils.GenerateRandomName("t")
 	}
 
-	// WC CIDRs have to not overlap and be in the 10.225. - 10.255. range, so
-	// we select a random number in that range and set it as the second octet.
-	//nolint:gosec
-	randomOctet := rand.Intn(30) + 225
-	cidrOctet := fmt.Sprintf("%d", randomOctet)
-	templateValues := &application.TemplateValues{
-		ClusterName:  clusterName,
-		Organization: orgName,
-		ExtraValues: map[string]string{
-			"CIDRSecondOctet": cidrOctet,
-		},
-	}
-
 	return application.NewClusterApp(clusterName, application.ProviderAzure).
 		WithOrg(organization.New(orgName)).
 		WithAppValues(
 
 			values.MustMergeValues(append([]string{basePrivateClusterValues}, clusterValuesOverrides...)...),
 			values.MustMergeValues(append([]string{basePrivateDefaultAppsValues}, defaultAppsValuesOverrides...)...),
-			templateValues,
+			&application.TemplateValues{
+				ClusterName:  clusterName,
+				Organization: orgName,
+			},
 		)
 }
 
