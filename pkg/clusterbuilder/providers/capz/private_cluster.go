4e613cd9a958de45738bd6293b0f13980891db7e
Puru <5674762+tuladhar@users.noreply.github.com>
2024-08-21 15:16:34 +0545

    Add CAPZ private cluster builder

---
+++ pkg/clusterbuilder/providers/capz/private_cluster.go
@@ -0,0 +1,65 @@
+package capz
+
+import (
+	_ "embed"
+	"fmt"
+	"math/rand"
+
+	"github.com/giantswarm/clustertest/pkg/application"
+	"github.com/giantswarm/clustertest/pkg/organization"
+	"github.com/giantswarm/clustertest/pkg/utils"
+
+	"github.com/giantswarm/cluster-standup-teardown/pkg/values"
+)
+
+var (
+	//go:embed values/private-cluster_values.yaml
+	basePrivateClusterValues string
+	//go:embed values/private-default-apps_values.yaml
+	basePrivateDefaultAppsValues string
+)
+
+// PrivateClusterBuilder is the private CAPZ ClusterBuilder
+type PrivateClusterBuilder struct {
+	CustomKubeContext string
+}
+
+// NewClusterApp builds a new private CAPZ cluster App
+func (c *PrivateClusterBuilder) NewClusterApp(clusterName string, orgName string, clusterValuesOverrides []string, defaultAppsValuesOverrides []string) *application.Cluster {
+	if clusterName == "" {
+		clusterName = utils.GenerateRandomName("t")
+	}
+	if orgName == "" {
+		orgName = utils.GenerateRandomName("t")
+	}
+
+	// WC CIDRs have to not overlap and be in the 10.225. - 10.255. range, so
+	// we select a random number in that range and set it as the second octet.
+	//nolint:gosec
+	randomOctet := rand.Intn(30) + 225
+	cidrOctet := fmt.Sprintf("%d", randomOctet)
+	templateValues := &application.TemplateValues{
+		ClusterName:  clusterName,
+		Organization: orgName,
+		ExtraValues: map[string]string{
+			"CIDRSecondOctet": cidrOctet,
+		},
+	}
+
+	return application.NewClusterApp(clusterName, application.ProviderAWS).
+		WithOrg(organization.New(orgName)).
+		WithAppValues(
+
+			values.MustMergeValues(append([]string{basePrivateClusterValues}, clusterValuesOverrides...)...),
+			values.MustMergeValues(append([]string{basePrivateDefaultAppsValues}, defaultAppsValuesOverrides...)...),
+			templateValues,
+		)
+}
+
+// KubeContext returns the known KubeConfig context that this builder expects
+func (c *PrivateClusterBuilder) KubeContext() string {
+	if c.CustomKubeContext != "" {
+		return c.CustomKubeContext
+	}
+	return "capz-private"
+}
