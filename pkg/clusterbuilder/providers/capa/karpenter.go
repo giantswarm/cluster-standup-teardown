b910fd8efcd250fc737f5e73b0b50755e1ea41e3
Jose Armesto <github@armesto.net>
2025-06-24 09:32:32 +0200

    Add karpenter cluster builder (#228)
    * Add karpenter cluster builder
    
    * Remove wrong comment

---
+++ pkg/clusterbuilder/providers/capa/karpenter.go
@@ -0,0 +1,52 @@
+package capa
+
+import (
+	_ "embed"
+
+	"github.com/giantswarm/cluster-standup-teardown/pkg/values"
+
+	"github.com/giantswarm/clustertest/pkg/application"
+	"github.com/giantswarm/clustertest/pkg/organization"
+	"github.com/giantswarm/clustertest/pkg/utils"
+)
+
+var (
+	//go:embed values/karpenter-cluster_values.yaml
+	baseKarpenterClusterValues string
+)
+
+// KarpenterBuilder is the CAPA KarpenterBuilder
+type KarpenterBuilder struct {
+	CustomKubeContext string
+}
+
+// NewClusterApp builds a new CAPA cluster App
+func (c *KarpenterBuilder) NewClusterApp(clusterName string, orgName string, clusterValuesOverrides []string, defaultAppsValuesOverrides []string) *application.Cluster {
+	if clusterName == "" {
+		clusterName = utils.GenerateRandomName("t")
+	}
+	if orgName == "" {
+		orgName = utils.GenerateRandomName("t")
+	}
+
+	return application.NewClusterApp(clusterName, application.ProviderAWS).
+		WithOrg(organization.New(orgName)).
+		WithAppValues(
+			values.MustMergeValues(append([]string{baseKarpenterClusterValues}, clusterValuesOverrides...)...),
+			"",
+			&application.TemplateValues{
+				ClusterName:  clusterName,
+				Organization: orgName,
+			},
+		)
+}
+
+// KubeContext returns the known KubeConfig context that this builder expects
+func (c *KarpenterBuilder) KubeContext() string {
+
+	if c.CustomKubeContext != "" {
+		return c.CustomKubeContext
+	}
+
+	return "capa-karpenter"
+}
